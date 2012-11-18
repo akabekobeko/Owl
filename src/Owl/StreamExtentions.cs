7d4fcceadfd72f59bb62dbbfbcf0f414fe75ebf1
akabeko <akabekobeko@gmail.com>
2012-11-18 22:49:24 +0900

    Changed: Remove unnecessary methods.
    refs #74

--- src/Owl/StreamExtentions.cs
+++ src/Owl/StreamExtentions.cs
@@ -11,76 +11,34 @@ namespace Owl
 		/// <summary>
 		/// 真偽値 ( 8 ビット ) を読み取り、ストリームの位置を進めます。
 		/// </summary>
-		/// <param name="reader">ストリーム。</param>
+		/// <param name="stream">ストリーム。</param>
 		/// <returns>読み取った値。</returns>
-		public static bool ReadBool( this Stream reader )
+		public static bool ReadBool( this Stream stream )
 		{
-			return reader.ReadBool( 1 );
-		}
-
-		/// <summary>
-		/// 真偽値を読み取り、ストリームの位置を進めます。
-		/// このメソッドでは指定されたバイト数分、ストリームを読み進めますが、
-		/// データとして取得される値は、読み取ったバイト配列の先頭部分から必要なデータサイズ分となります。
-		/// </summary>
-		/// <param name="reader">ストリーム。</param>
-		/// <param name="count">読み取るバイト数。</param>
-		/// <returns>読み取った値。</returns>
-		public static bool ReadBool( this Stream reader, int count )
-		{
-			return BitConverter.ToBoolean( reader.ReadBytes( count ), 0 );
+			return BitConverter.ToBoolean( stream.ReadBytes( 1 ), 0 );
 		}
 
 		/// <summary>
 		/// バイト配列を読み取り、ストリームの位置を進めます。
 		/// </summary>
-		/// <param name="reader">ストリーム。</param>
+		/// <param name="stream">ストリーム。</param>
 		/// <param name="count">読み取るバイト数。</param>
 		/// <returns>読み取った値。</returns>
-		public static byte[] ReadBytes( this Stream reader, int count )
+		public static byte[] ReadBytes( this Stream stream, int count )
 		{
 			var b = new byte[ count ];
-			reader.Read( b, 0, count );
+			stream.Read( b, 0, count );
 			return b;
 		}
 
 		/// <summary>
 		/// GUID ( 128 ビット ) を読み取り、ストリームの位置を進めます。
 		/// </summary>
-		/// <param name="reader">ストリーム。</param>
+		/// <param name="stream">ストリーム。</param>
 		/// <returns>読み取った値。</returns>
-		public static Guid ReadGuid( this Stream reader )
+		public static Guid ReadGuid( this Stream stream )
 		{
-			return reader.ReadGuid( 16 );
-		}
-
-		/// <summary>
-		/// GUID を読み取り、ストリームの位置を進めます。
-		/// このメソッドでは指定されたバイト数分、ストリームを読み進めますが、
-		/// データとして取得される値は、読み取ったバイト配列の先頭部分から必要なデータサイズ分となります。
-		/// </summary>
-		/// <param name="reader">ストリーム。</param>
-		/// <param name="count">読み取るバイト数。</param>
-		/// <returns>読み取った値。</returns>
-		/// <exception cref="ArgumentException">count に指定された数値が 16 バイトを下回っている</exception>
-		public static Guid ReadGuid( this Stream reader, int count )
-		{
-			var b = reader.ReadBytes( count );
-			if( count == 16 )
-			{
-				return new Guid( b );
-			}
-			else if( count > 16 )
-			{
-				// Guid のコンストラクタに指定するバイト配列は 16 ビットでなければならない
-				var data = new byte[ 16 ];
-				Array.Copy( b, 0, data, 0, 16 );
-				return new Guid( data );
-			}
-			else
-			{
-				throw new ArgumentException( "count" );
-			}
+			return new Guid( stream.ReadBytes( 16 ) );
 		}
 
 		/// <summary>
@@ -90,66 +48,27 @@ namespace Owl
 		/// <returns>読み取った値。</returns>
 		public static short ReadInt16( this Stream reader )
 		{
-			return reader.ReadInt16( 2 );
-		}
-
-		/// <summary>
-		/// 符号付き 16 ビット整数値を読み取り、ストリームの位置を進めます。
-		/// このメソッドでは指定されたバイト数分、ストリームを読み進めますが、
-		/// データとして取得される値は、読み取ったバイト配列の先頭部分から必要なデータサイズ分となります。
-		/// </summary>
-		/// <param name="reader">ストリーム。</param>
-		/// <param name="count">読み取るバイト数。</param>
-		/// <returns>読み取った値。</returns>
-		public static short ReadInt16( this Stream reader, int count )
-		{
-			return BitConverter.ToInt16( reader.ReadBytes( count ), 0 );
+			return BitConverter.ToInt16( reader.ReadBytes( 2 ), 0 );
 		}
 
 		/// <summary>
 		/// 符号付き 32 ビット整数値を読み取り、ストリームの位置を進めます。
 		/// </summary>
-		/// <param name="reader">ストリーム。</param>
+		/// <param name="stream">ストリーム。</param>
 		/// <returns>読み取った値。</returns>
-		public static int ReadInt32( this Stream reader )
+		public static int ReadInt32( this Stream stream )
 		{
-			return reader.ReadInt32( 4 );
-		}
-
-		/// <summary>
-		/// 符号付き 32 ビット整数値を読み取り、ストリームの位置を進めます。
-		/// このメソッドでは指定されたバイト数分、ストリームを読み進めますが、
-		/// データとして取得される値は、読み取ったバイト配列の先頭部分から必要なデータサイズ分となります。
-		/// </summary>
-		/// <param name="reader">ストリーム。</param>
-		/// <param name="count">読み取るバイト数。</param>
-		/// <returns>読み取った値。</returns>
-		public static int ReadInt32( this Stream reader, int count )
-		{
-			return BitConverter.ToInt32( reader.ReadBytes( count ), 0 );
+			return BitConverter.ToInt32( stream.ReadBytes( 4 ), 0 );
 		}
 
 		/// <summary>
 		/// 符号付き 64 ビット整数値を読み取り、ストリームの位置を進めます。
 		/// </summary>
-		/// <param name="reader">ストリーム。</param>
+		/// <param name="stream">ストリーム。</param>
 		/// <returns>読み取った値。</returns>
-		public static long ReadInt64( this Stream reader )
+		public static long ReadInt64( this Stream stream )
 		{
-			return reader.ReadInt64( 8 );
-		}
-
-		/// <summary>
-		/// 符号付き 64 ビット整数値を読み取り、ストリームの位置を進めます。
-		/// このメソッドでは指定されたバイト数分、ストリームを読み進めますが、
-		/// データとして取得される値は、読み取ったバイト配列の先頭部分から必要なデータサイズ分となります。
-		/// </summary>
-		/// <param name="reader">ストリーム。</param>
-		/// <param name="count">読み取るバイト数。</param>
-		/// <returns>読み取った値。</returns>
-		public static long ReadInt64( this Stream reader, int count )
-		{
-			return BitConverter.ToInt64( reader.ReadBytes( count ), 0 );
+			return BitConverter.ToInt64( stream.ReadBytes( 8 ), 0 );
 		}
 
 		/// <summary>
@@ -159,66 +78,27 @@ namespace Owl
 		/// <returns>読み取った値。</returns>
 		public static ushort ReadUInt16( this Stream stream )
 		{
-			return stream.ReadUInt16( 2 );
-		}
-
-		/// <summary>
-		/// 符号なし 16 ビット整数値を読み取り、ストリームの位置を進めます。
-		/// このメソッドでは指定されたバイト数分、ストリームを読み進めますが、
-		/// データとして取得される値は、読み取ったバイト配列の先頭部分から必要なデータサイズ分となります。
-		/// </summary>
-		/// <param name="reader">ストリーム。</param>
-		/// <param name="count">読み取るバイト数。</param>
-		/// <returns>読み取った値。</returns>
-		public static ushort ReadUInt16( this Stream reader, int count )
-		{
-			return BitConverter.ToUInt16( reader.ReadBytes( count ), 0 );
+			return BitConverter.ToUInt16( stream.ReadBytes( 2 ), 0 );
 		}
 
 		/// <summary>
 		/// 符号なし 32 ビット整数値を読み取り、ストリームの位置を進めます。
 		/// </summary>
-		/// <param name="reader">ストリーム。</param>
+		/// <param name="stream">ストリーム。</param>
 		/// <returns>読み取った値。</returns>
-		public static uint ReadUInt32( this Stream reader )
+		public static uint ReadUInt32( this Stream stream )
 		{
-			return reader.ReadUInt32( 4 );
-		}
-
-		/// <summary>
-		/// 符号なし 32 ビット整数値を読み取り、ストリームの位置を進めます。
-		/// このメソッドでは指定されたバイト数分、ストリームを読み進めますが、
-		/// データとして取得される値は、読み取ったバイト配列の先頭部分から必要なデータサイズ分となります。
-		/// </summary>
-		/// <param name="reader">ストリーム。</param>
-		/// <param name="count">読み取るバイト数。</param>
-		/// <returns>読み取った値。</returns>
-		public static uint ReadUInt32( this Stream reader, int count )
-		{
-			return BitConverter.ToUInt32( reader.ReadBytes( count ), 0 );
+			return BitConverter.ToUInt32( stream.ReadBytes( 4 ), 0 );
 		}
 
 		/// <summary>
 		/// 符号なし 64 ビット整数値を読み取り、ストリームの位置を進めます。
 		/// </summary>
-		/// <param name="reader">ストリーム。</param>
+		/// <param name="stream">ストリーム。</param>
 		/// <returns>読み取った値。</returns>
-		public static ulong ReadUInt64( this Stream reader )
+		public static ulong ReadUInt64( this Stream stream )
 		{
-			return reader.ReadUInt64( 8 );
-		}
-
-		/// <summary>
-		/// 符号なし 64 ビット整数値を読み取り、ストリームの位置を進めます。
-		/// このメソッドでは指定されたバイト数分、ストリームを読み進めますが、
-		/// データとして取得される値は、読み取ったバイト配列の先頭部分から必要なデータサイズ分となります。
-		/// </summary>
-		/// <param name="reader">ストリーム。</param>
-		/// <param name="count">読み取るバイト数。</param>
-		/// <returns>読み取った値。</returns>
-		public static ulong ReadUInt64( this Stream reader, int count )
-		{
-			return BitConverter.ToUInt64( reader.ReadBytes( count ), 0 );
+			return BitConverter.ToUInt64( stream.ReadBytes( 8 ), 0 );
 		}
 	}
 }
