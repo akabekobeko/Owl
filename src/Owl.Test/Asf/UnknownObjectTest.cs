15506b2164870ed882daa31e83ffa4bd8bb374be
akabeko <akabekobeko@gmail.com>
2012-11-18 22:16:14 +0900

    Added: UnknownObject test codxe
    refs #73

--- src/Owl.Test/Asf/UnknownObjectTest.cs
+++ src/Owl.Test/Asf/UnknownObjectTest.cs
@@ -12,6 +12,27 @@ namespace Owl.Test.Asf
 	public class UnknownObjectTest
 	{
 		/// <summary>
+		/// テスト用のデータ ストリームを生成します
+		/// </summary>
+		/// <returns>ストリーム。</returns>
+		private Stream CreateStream()
+		{
+			var guid = Guid.NewGuid();
+			var value = new byte[ 1024 ];
+			var src = new MemoryStream();
+
+			var b = guid.ToByteArray();
+			src.Write( b, 0, b.Length );
+
+			b = BitConverter.GetBytes( ( ulong )( ObjectHeader.ClassByteSize + value.Length ) );
+			src.Write( b, 0, b.Length );
+			src.Write( value, 0, value.Length );
+			src.Seek( 0, SeekOrigin.Begin );
+
+			return src;
+		}
+
+		/// <summary>
 		/// インスタンス生成をテストします。
 		/// </summary>
 		[Test]
@@ -30,6 +51,29 @@ namespace Owl.Test.Asf
 			Assert.Throws< ArgumentNullException >( () => { var obj = new UnknownObject( null, null   ); } );
 			Assert.Throws< ArgumentNullException >( () => { var obj = new UnknownObject( src,  null   ); } );
 			Assert.Throws< ArgumentNullException >( () => { var obj = new UnknownObject( null, header ); } );		
+		}
+
+		/// <summary>
+		/// 保存をテストします。
+		/// </summary>
+		[Test]
+		public void Save()
+		{
+			var src       = this.CreateStream();
+			var srcObject = new UnknownObject( src, new ObjectHeader( src ) );
+
+			// 書き込み後、ストリーム上の位置が終端となるため、ヘッダ読み込み用に先頭へ戻す
+			// Owl クライアントは ASF オブジェクトを直接操作しないため、通常は意識する必要のない処理
+			//
+			var dest = new MemoryStream();
+			srcObject.Save( src, dest );
+			dest.Seek( 0, SeekOrigin.Begin );
+
+			var destObject = new UnknownObject( dest, new ObjectHeader( dest ) );
+
+			// 複製が適切におこなわれたことをチェック
+			Assert.AreEqual( srcObject.Size, destObject.Size, "Size" );
+			Assert.AreEqual( srcObject.Id,   destObject.Id,   "ID"   );
 		}
 	}
 }
